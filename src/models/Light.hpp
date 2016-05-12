0bfd5d0aeabea2b4b2447327c26ad5d66d16451c
武慧凯 <HuikaiWu@icloud.com>
2016-05-12 17:08:28 +0800

    virtual transform for Light

--- src/models/Light.hpp
+++ src/models/Light.hpp
@@ -16,9 +16,7 @@ public:
 
 	virtual vec3 calcShade(const Scene *scene_ptr, const vec3 &hit_position, float &shade_idx) const = 0;
 
-	virtual void transform(const mat4 &transform_matrix){
-		mat4 a = transform_matrix;
-	}
+	virtual void transform(const mat4 &transform_matrix) = 0;
 
 	inline const vec3 &getEmissionColor() const {
 		return __emission_color;
